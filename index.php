3940cb63cafa0b687e1170caa844ce8c05ac1cce
Alejandro Alejo Rodriguez <alalro@floridauniversitaria.es>
2016-10-05 12:09:12 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+//fichero index.php
+//creado por Paco Gomez
+<html>
+  <head>
+      <title>ACTIVIDAD 03: Git y PHP</title>
+      <?php
+        echo "Alejandro"."<br>";
+        echo "Ajenjo"."<br>";
+        print "Rodriguez";
+      ?>
+  </head>
+  <body>
+</html>
